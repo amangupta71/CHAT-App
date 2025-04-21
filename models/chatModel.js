9cbbc3aa1ca7c24d485dd6a08b48873c1f35ad34
Aman <6322aman@gmail.com>
2025-04-21 12:44:59 +0530

    registration

---
+++ models/chatModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema({
+    sender_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    reciver_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    message:{
+        type:String,
+        required:true
+    }
+},
+{ timestamp:true }
+);
+
+module.exports = mongoose.model('Chat',chatSchema)
